2568f71268ee95beaf6eb2912ec0df4abb20aab2
Mads Rumle Nordstrom <Mads Rumle Nordstrom@DESKTOP-RPC3V5J>
2021-06-11 11:35:45 +0200

    Timer = 24 fps

--- Src/main.c
+++ Src/main.c
@@ -17,10 +17,6 @@
 #define FIX14_MULT(a,b) (((a)*(b)) >> FIX14_SHIFT)
 #define FIX14_DIV(a,b) (((a)<<FIX14_SHIFT)/b)
 
-#define FIX16_SHIFT 16
-#define FIX16_MULT(a,b) (((a)*(b)) >> FIX16_SHIFT)
-#define FIX16_DIV(a,b) (((a)<<FIX16_SHIFT)/b)
-
 int main(void){
 	// Initialization
 	uart_init(115200);
@@ -32,34 +28,9 @@ int main(void){
 	lcd_init();
 	initTimer();
 	// drawWindowNoTitle(1,1,255,127,1);
-	int32_t count = 0;
 	while(1){
 		if (global == 1){
-			switch(count){
-			case 0:
-				printf("0");
-			break;
-			case 1:
-				printf("1");
-			break;
-			case 2:
-				printf("2");
-			break;
-			case 3:
-				printf("3");
-			break;
-			case 4:
-				printf("4");
-			break;
-			case 5:
-				printf("5");
-			break;
-			}
-			if(count == 5){
-				count = 0;
-			} else {
-				count++;
-			}
+
 			global = 0;
 		}
 
