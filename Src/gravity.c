e44f12cd27687217259c99e45024868469f75c5a
Bruger <Bruger@10.16.163.225>
2021-06-11 11:39:59 +0200

    Gravity

---
+++ Src/gravity.c
@@ -0,0 +1,20 @@
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" 		// Input/output library for this course
+#include "stdio.h"
+#include "stdint.h"
+#include "main.h"
+#include <string.h>
+#include <math.h>
+
+struct gravity {
+	int16_t x, y;
+};
+
+struct gravity *g;
+
+void force (*g) {
+	s =
+	device vector
+	int32_t dist = sqrt((asteroid.x - player.x)^2 + (asteroid.y - player.y)^2);
+	double d = 1/2 * a * s^2 + v0 * s + d0
+}
